9bbd8aedf1701cc6a63391f6a1f1127277f61409
MBehtemam <ehtemam@gmail.com>
2019-01-04 20:33:10 +0330

    adding comments for price formatter

--- Helpers/PriceFormatter.js
+++ Helpers/PriceFormatter.js
@@ -1,5 +1,6 @@
 /**
- * this method get
+ * this method get cents and convert it to dollar format
+ * @param {Number} cents cents price of product
  */
 const priceFormatter = cents =>
   new Intl.NumberFormat("en-US", {
