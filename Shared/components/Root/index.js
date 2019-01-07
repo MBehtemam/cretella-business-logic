c54dab34d2942bf019cb9bef8182eee611c09dce
MBehtemam <ehtemam@gmail.com>
2019-01-08 00:02:21 +0330

    add logger and enhancers

--- Shared/components/Root/index.js
+++ Shared/components/Root/index.js
@@ -3,9 +3,15 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../../../Reducers";
 import thunkMiddleware from "redux-thunk";
+import loggerMiddleware from "../../../middleweare/logger";
+import monitorReducerEnhancer from "../../../enhancers/monitorReducer";
 
-const middlewareEnhancer = applyMiddleware(thunkMiddleware);
-const composedEnhancers = compose(middlewareEnhancer);
+const middlewareEnhancer = applyMiddleware(loggerMiddleware, thunkMiddleware);
+const composedEnhancers = compose(
+  middlewareEnhancer,
+  monitorReducerEnhancer
+);
+
 const store = createStore(rootReducer, undefined, composedEnhancers);
 
 export default class Root extends Component {
