eb922982d9332bf68ea9c14a19615237e3fce2ac
MBehtemam <ehtemam@gmail.com>
2019-01-08 12:01:56 +0330

    bug fixing of limit action

--- Actions/LimitActions.js
+++ Actions/LimitActions.js
@@ -3,7 +3,7 @@ import { fetchProducts } from "./ProductsActions";
 export const setLimitProducts = (page, limit, sortBy) => {
   return dispatch => {
     dispatch(fetchProducts(true, page, limit, sortBy));
-    dispatch(_setLimitProducts());
+    dispatch(_setLimitProducts(limit));
   };
 };
 
