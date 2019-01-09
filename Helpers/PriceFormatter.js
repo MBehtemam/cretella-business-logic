c7aa253aac1c44b20945c9da2af3387bb24c23da
MBehtemam <ehtemam@gmail.com>
2019-01-09 06:33:02 +0330

    making pure cent to dollar formatter

--- Helpers/PriceFormatter.js
+++ Helpers/PriceFormatter.js
@@ -2,10 +2,6 @@
  * this method get cents and convert it to dollar format
  * @param {Number} cents cents price of product
  */
-const priceFormatter = cents =>
-  (cents / 100).toLocaleString("en-US", {
-    style: "currency",
-    currency: "USD"
-  });
+const priceFormatter = cents => `$${parseFloat(cents / 100).toFixed(2)}`;
 
 export default priceFormatter;
